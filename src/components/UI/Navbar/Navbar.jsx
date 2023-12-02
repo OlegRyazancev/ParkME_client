6534d67971fe075f45fee8691ae5d6ea9a37e621
Oleg Ryazancev <oleg.ryazancev1022@gmail.com>
2023-12-02 22:55:43 +0300

    refactor navbar

--- src/components/UI/Navbar/Navbar.jsx
+++ src/components/UI/Navbar/Navbar.jsx
@@ -10,11 +10,11 @@ const Navbar = () => {
     return (
         <div className={cl.navbar}>
             <img src={logo} alt="logo" className={cl.navbar__logo}/>
-            <div style={{backgroundColor: 'transparent'}}>
+            <div className={cl.btnContainer}>
                 <Link className={cl.navbar__links} to="/home">HOME</Link>
                 <Link className={cl.navbar__links} to="/about">ABOUT</Link>
                 <Link className={cl.navbar__links} to="/profile">PROFILE</Link>
-                <button onClick={() => store.logout()}>Logout</button>
+                <button className={cl.btn} onClick={() => store.logout()}>LOGOUT</button>
             </div>
         </div>
     );
