d60e802d4b7dd589bbc5ed8524be9f277aa1ac29
Oleg Ryazancev <oleg.ryazancev1022@gmail.com>
2023-12-11 19:37:06 +0300

    fix sorting reservations by status

--- src/hooks/useReservations.js
+++ src/hooks/useReservations.js
@@ -3,7 +3,19 @@ import {useMemo} from "react";
 export const useReservations = (reservations, sort) => {
     return useMemo(() => {
         if (sort) {
-            return [...reservations].sort((a, b) => b[sort].localeCompare(a[sort]))
+            return [...reservations].sort((a, b) => {
+                const statusOrder = {
+                    'PLANNED': 1,
+                    'ACTIVE': 2,
+                    'COMPLETED': 3,
+                    'CANCELED': 4
+                };
+
+                const statusA = a[sort];
+                const statusB = b[sort];
+
+                return statusOrder[statusA] - statusOrder[statusB];
+            });
         }
         return reservations;
     }, [sort, reservations]);
