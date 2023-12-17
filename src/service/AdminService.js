61445fc13c38f1a905e3e15aa3a1b5fb6bfb7406
Oleg Ryazancev <oleg.ryazancev1022@gmail.com>
2023-12-17 22:08:28 +0300

    create adminService

---
+++ src/service/AdminService.js
@@ -0,0 +1,41 @@
+import $api from "../http";
+
+export default class AdminService {
+
+    static async getAllCars() {
+        return await $api.get('/admin/cars');
+    }
+
+    static async getAllUsers() {
+        return await $api.get('/admin/users');
+    }
+
+    static async getAllReservations() {
+        return await $api.get('/admin/reservations');
+    }
+
+    static async createZone(zoneData) {
+        return await $api.post('/admin/zones', zoneData);
+    }
+
+    static async updateZone(zoneData) {
+        return await $api.put('/admin/zones', zoneData);
+    }
+
+    static async createPlacesInZone(zoneId, countPlaces) {
+        return await $api.post(`/admin/zones/${zoneId}/places?places=${countPlaces}`);
+    }
+
+    static async changePlaceStatusById(placeId, status) {
+        return await $api.put(`/admin/places/${placeId}/status?status=${status}`);
+    }
+
+    static async deleteZoneWithPlaces(zoneId) {
+        return await $api.delete(`/admin/zones/${zoneId}`);
+    }
+
+    static async deletePlace(placeId) {
+        return await $api.delete(`/admin/places/${placeId}`);
+    }
+
+}
