2e262cc53c028c22603da6e2c3bd91416e42ac36
Oleg Ryazancev <oleg.ryazancev1022@gmail.com>
2024-03-04 18:22:49 +0300

    add another log to check docker profile

--- src/http/index.js
+++ src/http/index.js
@@ -4,14 +4,13 @@ let apiUrl = `http://localhost:8080/api/v1`
 
 if (process.env.PROFILE_ACTIVE === `docker`) {
     apiUrl = `http://${process.env.BACKEND_IMAGE}:${process.env.BACKEND_PORT}/api/v1`
+    console.log('docker profile');
 }
-
 export const API_URL = apiUrl
+
 const $api = axios.create({
     baseURL: API_URL
 })
-
-console.log(apiUrl);
 
 $api.interceptors.request.use((config) => {
     config.headers.Authorization = `Bearer ${localStorage.getItem('accessToken')}`
